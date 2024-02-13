fcb480f0569d76fe6aa4fd2d0b9dfdc4b973ec5e
alitarichi <tarichiali@gmail.com>
2024-02-13 13:18:50 +0300

    2666 commit

---
+++ 2666. Allow One Function Call/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+
+    let hasBeenCalled = false;
+    let result;
+  
+    return function(...args) {
+      if (!hasBeenCalled) {
+        result = fn(...args);
+        hasBeenCalled = true;
+        return result;
+      } else {
+        return undefined;
+      }
+    }
+  
+  };
+  
+  
