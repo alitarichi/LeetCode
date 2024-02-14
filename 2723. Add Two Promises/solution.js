e443c24d914ff76794ebdc689d67eb9123efc420
alitarichi <tarichiali@gmail.com>
2024-02-14 13:49:33 +0300

    2723 commit

---
+++ 2723. Add Two Promises/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const result1 = await promise1;
+    const result2 = await promise2;
+
+    return result1 + result2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
